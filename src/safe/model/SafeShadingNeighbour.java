864712aa678a4b6760cfba9de7388fd7cccc76c1
LECOMTE-ISADEV <44645063+LECOMTE-ISADEV@users.noreply.github.com>
2025-03-07 14:24:57 +0100

    Add files via upload

--- src/safe/model/SafeShadingNeighbour.java
+++ src/safe/model/SafeShadingNeighbour.java
@@ -1,7 +1,7 @@
 /** 
  * Hi-SAFE : A 3D Agroforestry Model for Integrating Dynamic Tree–Crop Interactions
  * 
- * Copyright (C) 2000-2024 INRAE 
+ * Copyright (C) 2000-2025 INRAE 
  * 
  * Authors  
  * C.DUPRAZ       	- INRAE Montpellier France
@@ -17,18 +17,26 @@
  * D.HARJA			- ICRAF Bogor Indonisia
  * 
  * This file is part of Hi-SAFE  
- * Hi-SAFE is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
+ * Hi-SAFE is free software under the terms of the CC-BY License as published by the Creative Commons Corporation
  *
- * Hi-SAFE is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
- * See the GNU Lesser General Public License for more details.
+ * You are free to:
+ *		Share — copy and redistribute the material in any medium or format for any purpose, even commercially.
+ *		Adapt — remix, transform, and build upon the material for any purpose, even commercially.
+ *		The licensor cannot revoke these freedoms as long as you follow the license terms.
+ * 
+ * Under the following terms:
+ * 		Attribution — 	You must give appropriate credit , provide a link to the license, and indicate if changes were made . 
+ *               		You may do so in any reasonable manner, but not in any way that suggests the licensor endorses you or your use.
+ *               
+ * 		No additional restrictions — You may not apply legal terms or technological measures that legally restrict others from doing anything the license permits.
+ *               
+ * Notices:
+ * 		You do not have to comply with the license for elements of the material in the public domain or where your use is permitted 
+ *      by an applicable exception or limitation .
+ *		No warranties are given. The license may not give you all of the permissions necessary for your intended use. 
+ *		For example, other rights such as publicity, privacy, or moral rights may limit how you use the material.  
  *
- * You should have received a copy of the GNU lesser General Public License
- * If not, see <http://www.gnu.org/licenses/>.
+ * For more details see <https://creativecommons.org/licenses/by/4.0/>.
  *
  */
 
